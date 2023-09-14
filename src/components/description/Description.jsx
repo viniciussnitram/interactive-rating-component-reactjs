6ce775ab0b47efe667e1b8ee0afd8c40d56341b5
viniciussnitram <vinicius.martins.vsm@outlook.com>
2023-09-13 23:51:58 -0300

    style: Increased the font-size of h1 tag

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -1,5 +1,12 @@
 export function Description() {
     return (
-        <p className="bg-red-100">Oi</p>
+        <section>
+            <h1 className="text-3xl">How did we do?</h1>
+            <p>
+                Please let us know how we did with your support
+                request. All feedback is appreciated to help us
+                improve our offering!
+            </p>
+        </section>
     );
 }
