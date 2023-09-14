2b64b35c306e29a124c9225c96f6dc90e7a8a6e0
viniciussnitram <vinicius.martins.vsm@outlook.com>
2023-09-13 23:51:02 -0300

    style: Added TailwindCSS to the buttons of rates and props. Now we have each button with a unique value (1 to 5). They are rounded full, background-color grey. When hovers mouse: background-color light-grey, text white. When focus mouse: background-color orange and text white

--- src/components/rate/Rate.jsx
+++ src/components/rate/Rate.jsx
@@ -1,5 +1,9 @@
-export function Rate() {
+export function Rate(props) {
+    let id = props.id;
+
     return (
-        <h1>Oi</h1>
+        <>
+            <input className="w-[50px] border-solid border-1 border-white rounded-full text-medium-grey p-3 bg-dark-blue font-bold hover:bg-light-grey hover:text-white hover:cursor-pointer focus:bg-orange focus:text-white" type="button" name="rate" value={id} />
+        </>
     );
 }
