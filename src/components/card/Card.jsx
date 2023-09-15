c2f7513009a2b72e3c4f78f64dc2230ebc83ba27
viniciussnitram <vinicius.martins.vsm@outlook.com>
2023-09-15 16:10:10 -0300

    style and refactor: The Card Component is done for now. The width is good, changed background-color, border-radius, positioning

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,17 +6,19 @@ import { Submit } from '../submit/Submit';
 export function Card() {
     return (
         <>
-            <main className="w-[400px] p-[45px] bg-very-dark-blue flex flex-col gap-5">
+            <main className="bg-very-dark-blue flex flex-col gap-5 m-auto p-[45px] rounded-3xl w-[450px]">
                 <Logo />
                 <Description />
-                <div className="flex gap-5">
-                    <Rate />
-                    <Rate />
-                    <Rate />
-                    <Rate />
-                    <Rate />
-                </div>
-                <Submit />
+                <form className="flex flex-col gap-5">
+                    <div className="flex gap-5">
+                        <Rate value={1} />
+                        <Rate value={2} />
+                        <Rate value={3} />
+                        <Rate value={4} />
+                        <Rate value={5} />
+                    </div>
+                    <Submit />
+                </form>
             </main>
         </>
     );
