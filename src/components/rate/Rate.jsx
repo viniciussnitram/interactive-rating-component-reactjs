ebbb880fdbfe3f58db8476cab50d6c64bc9f5756
viniciussnitram <vinicius.martins.vsm@outlook.com>
2023-09-15 23:25:41 -0300

    refactor and style: The Rate button Component  is done for now. Added background-color, padding, positioning, and put the classnames alphabet order in tailwindcss

--- src/components/rate/Rate.jsx
+++ src/components/rate/Rate.jsx
@@ -3,7 +3,7 @@ export function Rate(props) {
 
     return (
         <>
-            <input className="bg-dark-blue font-bold py-4 px-6 rounded-full text-medium-grey focus:bg-orange focus:text-white hover:bg-light-grey hover:cursor-pointer hover:text-white" name="rate" type="button" value={value} />
+            <input className="bg-dark-blue font-bold h-[54px] p-4 rounded-full text-medium-grey w-[54px] focus:bg-orange focus:text-white hover:bg-light-grey hover:cursor-pointer hover:text-white" name="rate" type="button" value={value} />
         </>
     );
 }
