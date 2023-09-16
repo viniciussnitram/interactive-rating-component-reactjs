85dfe12c548602a31dfa294c7ce7f18c1d457194
viniciussnitram <vinicius.martins.vsm@outlook.com>
2023-09-15 21:35:28 -0300

    refactor and style: The Description Component is done for now. Added background-color, padding, positioning, font-size and put the classnames alphabet order in tailwindcss

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -1,8 +1,8 @@
 export function Description() {
     return (
-        <section>
-            <h1 className="text-3xl">How did we do?</h1>
-            <p>
+        <section className="flex flex-col gap-3">
+            <h1 className="font-semibold text-3xl">How did we do?</h1>
+            <p className="text-light-grey">
                 Please let us know how we did with your support
                 request. All feedback is appreciated to help us
                 improve our offering!
