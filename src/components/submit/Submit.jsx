148794b1a6a2146e4c6731753ff2adcaa0290682
viniciussnitram <vinicius.martins.vsm@outlook.com>
2023-09-15 21:11:14 -0300

    refactor and style: The Submit button Card is done for now. Added background-color, padding, border-radius and put the classnames alphabet order in tailwindcss

--- src/components/submit/Submit.jsx
+++ src/components/submit/Submit.jsx
@@ -1,5 +1,5 @@
 export function Submit() {
     return (
-        <h1>Oi</h1>
+        <input className="bg-orange font-bold leading-7 p-2 rounded-full tracking-[2px] uppercase w-[100%] hover:bg-white hover:cursor-pointer hover:text-orange" type="submit" value="submit" />
     );
 }
