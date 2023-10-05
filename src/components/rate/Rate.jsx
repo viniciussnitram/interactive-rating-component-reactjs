44be9462cf8e88bccaf26fa91f4a3f05cb79639a
viniciussnitram <vinicius.martins.vsm@outlook.com>
2023-10-04 21:15:27 -0300

    refactor: Deleted all the unnecessaries components

--- src/components/rate/Rate.jsx
+++
@@ -1,9 +0,0 @@
-export function Rate(props) {
-    let value = props.value;
-
-    return (
-        <>
-            <input className="bg-dark-blue font-bold h-[54px] p-4 rounded-full text-medium-grey w-[54px] focus:bg-orange focus:text-white hover:bg-light-grey hover:cursor-pointer hover:text-white" name="rate" type="button" value={value} />
-        </>
-    );
-}
